693fc381060c28a423b384f6105ac2ba3d7ae95c
somename <someemail>
2010-10-26 17:23:26 -0200

    besouro automatic message

--- src/Romans.java
+++ src/Romans.java
@@ -13,10 +13,10 @@ public class Romans {
 			list.add(d);
 		}
 		
-		System.out.println(list.size());
 		
 		int n=0;
 		for (int i=0 ; i<list.size() ; i++) {
+			System.out.println("--");
 			if (i+1 < list.size()) {
 				if (list.get(i) < list.get(i+1)) {
 					n -= list.get(i);
