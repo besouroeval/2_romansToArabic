5d0427c8511d16b1c0d110215ae846403457e53c
somename <someemail>
2010-10-26 17:20:09 -0200

    besouro automatic message

--- src/Romans.java
+++ src/Romans.java
@@ -6,12 +6,18 @@ public class Romans {
 
 	public static int toArabic(String string) {
 		int n=0;
+		
 		List<Integer> list = new ArrayList<Integer>();
 		
 		for (int i=0 ; i<string.length() ; i++) {
 			int d = translateDigit(string.charAt(i));
+			list.add(d);
+		}
+		
+		for (int i=0 ; i<string.length() ; i++) {
 			n += d;
 		}
+		
 		return n;
 	}
 
