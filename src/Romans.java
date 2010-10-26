d6388a0da7744a7288b86cca5434dee7d319f037
somename <someemail>
2010-10-26 17:10:28 -0200

    besouro automatic message

--- src/Romans.java
+++ src/Romans.java
@@ -2,9 +2,11 @@
 public class Romans {
 
 	public static int toArabic(String string) {
+		if (string.equals("I"))
+			return 1;
 		if (string.equals("V"))
 			return 5;
-		return 1;
+		return -1;
 	}
 
 }
